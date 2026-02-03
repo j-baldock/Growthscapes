d17cba6ac62e1d63e704f0b62153d267ecd4a638
Jeff Baldock <baldockj@oregonstate.edu>
2026-02-02 17:22:16 -0700

    first render

--- build_book.R
+++ build_book.R
@@ -6,11 +6,4 @@ quarto::quarto_render(output_format = "html")
 
 # render single chapters
 quarto::quarto_render("index.qmd", output_format = "html")
-quarto::quarto_render("DataViz.qmd", output_format = "html")
-quarto::quarto_render("Breakpoints.qmd", output_format = "html")
-quarto::quarto_render("FormatData.qmd", output_format = "html")
-quarto::quarto_render("ModelTemp-Letcher.qmd", output_format = "html")
-quarto::quarto_render("ModelTemp-EcoDrought.qmd", output_format = "html")
-# quarto::quarto_render("ModelTemp-EcoDrought-StdBasin.qmd", output_format = "html")
-# quarto::quarto_render("ModelTemp-EcoDrought-withCovs.qmd", output_format = "html", cache_refresh = TRUE)
-quarto::quarto_render("ModelTemp-EcoDrought-ByMonth.qmd", output_format = "html", cache_refresh = TRUE)
+

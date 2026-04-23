f896422c63bd101fefbc99f770f2154cda4b67ac
Jeff Baldock <baldockj@oregonstate.edu>
2026-04-23 14:51:43 -0600

    add dayofsim, extend sim duration, etc

--- build_book.R
+++ build_book.R
@@ -6,5 +6,7 @@ quarto::quarto_render(output_format = "html")
 
 # render single chapters
 quarto::quarto_render("index.qmd", output_format = "html")
-quarto::quarto_render("Functions.qmd", output_format = "html")
+quarto::quarto_render("Habitat.qmd", output_format = "html", cache_refresh = TRUE)
+quarto::quarto_render("Functions.qmd", output_format = "html", cache_refresh = TRUE)
 quarto::quarto_render("GrowthRegime.qmd", output_format = "html", cache_refresh = TRUE)
+quarto::quarto_render("IBM_simulation.qmd", output_format = "html", cache_refresh = TRUE)
